8ae4af3b8636033e4c4c8d56e668601f6c7ef7fb
Muukii <muukii.app@gmail.com>
2021-12-12 15:16:45 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -1,11 +1,11 @@
-// swift-tools-version:5.5
+// swift-tools-version:5.4
 import PackageDescription
 
 let package = Package(
   name: "CompositionKit",
   platforms: [.iOS(.v12)],
   products: [
-    .library(name: "CompositionKit", type: .static, targets: ["CompositionKit"]),
+    .library(name: "CompositionKit", targets: ["CompositionKit"]),
   ],
   dependencies: [
     .package(url: "https://github.com/muukii/MondrianLayout.git", from: "0.5.0")
