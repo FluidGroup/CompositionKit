b6cf9bf15d6c6f4e1be45c9e946623f329f9022c
Muukii <muukii.app@gmail.com>
2022-06-13 12:59:57 +0900

    :evergreen_tree: Update

--- Package.swift
+++ Package.swift
@@ -6,11 +6,9 @@ let package = Package(
   platforms: [.iOS(.v12)],
   products: [
     .library(name: "CompositionKit", targets: ["CompositionKit"]),
-    .library(name: "CompositionKitVerge", targets: ["CompositionKitVerge"]),
   ],
   dependencies: [
     .package(url: "https://github.com/muukii/MondrianLayout.git", from: "0.8.0"),
-    .package(name: "Verge", url: "https://github.com/VergeGroup/Verge", from: "8.14.0"),
     .package(url: "https://github.com/muukii/Descriptors.git", from: "0.2.1"),
   ],
   targets: [
@@ -18,9 +16,5 @@ let package = Package(
       name: "CompositionKit",
       dependencies: ["MondrianLayout", "Descriptors"]
     ),
-    .target(
-      name: "CompositionKitVerge",
-      dependencies: ["Verge", "CompositionKit"]
-    )
   ]
 )
